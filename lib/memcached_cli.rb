3e7494ad965bac50bb05aa18fd97b8fbda79ba90
Ming Liu <liuming@lmws.net>
2014-03-06 12:33:14 -0800

    draft implementation

--- lib/memcached_cli.rb
+++ lib/memcached_cli.rb
@@ -1,5 +1,19 @@
-require "memcached_cli/version"
+require "thor"
+require 'dalli'
+require_relative "memcached_cli/version"
 
 module MemcachedCli
-  # Your code goes here...
+  class Commands < Thor
+    MEMCACHED_COMMANDS = [ ]
+
+    CLIENT = Dalli::Client.new
+
+    Dalli::Client.instance_methods(false).each do |command|
+      params = Dalli::Client.instance_method(command).parameters.inspect
+      desc command.to_s, command.to_s
+      define_method command do |*params|
+        puts CLIENT.send(command, *params)
+      end
+    end
+  end
 end
