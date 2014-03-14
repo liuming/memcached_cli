1a7c84c363f3b85b53f448ba0de880fcba4380f5
Ming Liu <liuming@lmws.net>
2014-03-14 13:31:58 -0700

    remove MEMCACHED_COMMANDS; add servers option

--- lib/memcached_cli.rb
+++ lib/memcached_cli.rb
@@ -3,15 +3,15 @@ require 'dalli'
 require_relative "memcached_cli/version"
 
 module MemcachedCli
-  class Commands < Thor
-    MEMCACHED_COMMANDS = [
-      :add, :append, :cas, :decr, :delete, :flush, :flush_all, :get, :get_multi,
-      :incr, :prepend, :replace, :reset_stats, :set, :stats, :touch, :version
-    ]
+  class Commands < ::Thor
+    class_option :servers, type: :string
 
     CLIENT = Dalli::Client.new
 
-    MEMCACHED_COMMANDS.each do |command|
+    [
+      :add, :append, :cas, :decr, :delete, :flush, :flush_all, :get, :get_multi,
+      :incr, :prepend, :replace, :reset_stats, :set, :stats, :touch, :version
+    ].each do |command|
       params = Dalli::Client.instance_method(command).parameters
       desc command.to_s, params.reduce([]){|s, (required,name)| s << (required == :req ? name : "[#{name.to_s}]").to_s}.join(' ')
       define_method command do |*params|
